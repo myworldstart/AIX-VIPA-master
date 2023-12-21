8587b07a1b1da8afcaada7388c5d290dd01d9be3
peiki99 <14752799935@163.com>
2023-12-21 16:14:00 +0800

    add yolov5 backend

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ def index():
 
 
 # todo: can't move to top of this file?
-from . import anno_auto_test, anno_auto_train, data_info, eiseg_click, anno_auto_tradition, sam_click, sam_predict
+from . import anno_auto_test, anno_auto_train, data_info, eiseg_click, anno_auto_tradition, sam_click, sam_predict, yolo_detect
 
 # register blueprint here
 app.register_blueprint(anno_auto_test.bp)
@@ -27,6 +27,7 @@ app.register_blueprint(eiseg_click.bp)
 app.register_blueprint(anno_auto_tradition.bp)
 app.register_blueprint(sam_click.bp)
 app.register_blueprint(sam_predict.bp)
+app.register_blueprint(yolo_detect.bp)
 
 # load models
 seek_gpu()
